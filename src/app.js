7e9eaec686b8929b23e8c251591951a04ed228f0
Jason McGloin <jason.mcgloin@gmail.com>
2018-02-19 23:00:18 -0500

    removed test data

--- src/app.js
+++ src/app.js
@@ -17,17 +17,6 @@ import { setTextFilter } from "./actions/filters";
 // ===STORE=== //
 const store = configureStore();
 
-store.subscribe(() => {
-	const state = store.getState();
-	const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-	console.log(visibleExpenses);
-})
-
-// ===ACTIONS=== //
-const expenseOne = store.dispatch(addExpense({description: "Water Bill", amount: 5000, createdAt: 2750}));
-const expenseTwo = store.dispatch(addExpense({description: "Electric Bill", amount: 8000, createdAt: 3500}));
-const expenseThree = store.dispatch(addExpense({description: "Gas Bill", amount: 6000, createdAt: 1500}));
-
 // ===JSX=== //
 const jsx = (
 	<Provider store={store} >
