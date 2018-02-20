c39a9aa75fcc2dcc7c41f67794a8a86bc9d62505
Jason McGloin <jason.mcgloin@gmail.com>
2018-02-20 10:32:13 -0500

    seperate css and js and set up dev vs prod source maps

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -8,7 +8,7 @@ const ExpenseListItem =  ({ description, id, amount, createdAt, note }) => {
 			<h3>{ description }</h3>
 			<p>
 				Amount: { amount }   |
-				Date Created: { moment(createdAt).format("ddd, MMM Do, YYYY [at] h:mm a") }				
+				Date Created: { moment(createdAt).format("ddd, MMM Do, YYYY [at] h:mm A ") }
 				<Link to={`/edit/${id}`} >
 					Edit
 				</Link>
